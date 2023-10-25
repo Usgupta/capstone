898b357a32d55de0bb1c1c75d60a4f4409d9416c
Enigmayc <mayucong37@gmail.com>
2023-10-25 18:32:20 +0800

    basic results page added

--- frontend/app/results/page.tsx
+++ frontend/app/results/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+import Dropdown from "@/components/Dropdown";
+import Link from "next/link";
+
 export default function Results() {
-    return (
-      <>
-          <h1>Results</h1>
-      </>
-    )
-  }
-  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center">
+        <p className="text-2xl">89% confidence level</p>
+        <div className="flex justify-center mt-6">
+          <Link href="/">
+            <button className="bg-blue-300 btn ml-5 min-h-min h-[41px] rounded-lg">
+              Upload another file
+            </button>
+          </Link>
+          <div className="flex justify-center mr-6"></div>
+          <Dropdown />
+        </div>
+      </div>
+    </div>
+  );
+}
