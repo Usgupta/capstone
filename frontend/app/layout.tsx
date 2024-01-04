efe6d9c42752c2aa3f734df68ea31979ea73633d
Leecz888 <leecz888@hotmail.com>
2024-01-04 22:07:06 +0800

    removed unnecessary files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 // Components
-import Navbar from '../components/Navbar'
+import Navbar from '@/components/Navbar'
 
 export const metadata: Metadata = {
   title: 'Deepfake Detector',
